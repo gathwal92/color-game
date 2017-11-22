1b2bdf1ac7dd1e78ab96a28587dfa7238277a262
Gurpreet Athwal <gathwal92@icloud.com>
2017-11-21 21:27:50 -0500

    Able to match colors

--- colorGame.js
+++ colorGame.js
@@ -16,15 +16,16 @@ colorDisplay.textContent = pickedColor;
 for(var i = 0; i < squares.length; i++){
     //add colors to squares
     squares[i].style.backgroundColor = colors[i];
+    
     //add click listeners to squares
     squares[i].addEventListener("click", function(){
         //grab color of clicked square
-        var clickedColor = this.style.background;
+        var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
-        if(clickedColor === pickedColor) {
-            alert("Matched!");
+        if(clickedColor === pickedColor){
+            window.alert("Matched!")
         } else {
-            alert("Wrong!")
+            window.alert("Wrong!")
         }
     });
 }
